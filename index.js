cdd55f16f77ce3a8fbf450d9e2d7e919492a89ee
Harshit Gowda <68541048+baked1995@users.noreply.github.com>
2022-01-25 12:28:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,131 @@
+const videoCardContainer = document.querySelector('.video-container');
+
+let api_key = "AIzaSyDggutCJKk0yn9tNEWHb1EdNhl1aue0Nak";
+let video_http = "https://www.googleapis.com/youtube/v3/videos?";
+let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
+
+fetch(video_http + new URLSearchParams({
+    key: api_key,
+    part: 'snippet',
+    chart: 'mostPopular',
+    maxResults: 10,
+    regionCode: 'IN'
+}))
+.then(res => res.json())
+.then(data => {
+    data.items.forEach(item => {
+        getChannelIcon(item);
+    })
+})
+.catch(err => console.log(err));
+
+const getChannelIcon = (video_data) => {
+    fetch(channel_http + new URLSearchParams({
+        key: api_key,
+        part: 'snippet',
+        id: video_data.snippet.channelId
+    }))
+    .then(res => res.json())
+    .then(data => {
+        video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+        makeVideoCard(video_data);
+    })
+}
+
+const makeVideoCard = (data) => {
+    videoCardContainer.innerHTML += `
+    <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
+        <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
+        <div class="content">
+            <img src="${data.channelThumbnail}" class="channel-icon" alt="">
+            <div class="info">
+                <h4 class="title">${data.snippet.title}</h4>
+                <p class="channel-name">${data.snippet.channelTitle}</p>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+// search bar
+
+const searchInput = document.querySelector('.search-bar');
+const searchBtn = document.querySelector('.search-btn');
+let searchLink = "https://www.youtube.com/results?search_query=";
+
+searchBtn.addEventListener('click', () => {
+    if(searchInput.value.length){
+        location.href = searchLink + searchInput.value;
+    }
+})
+
+var GoogleAuth; // Google Auth object.
+function initClient() {
+  gapi.client.init({
+      'apiKey': 'AIzaSyDggutCJKk0yn9tNEWHb1EdNhl1aue0Nak',
+      'clientId': '204547197160-9k6jrkdeeg9asd34i0uctsajgvfktskl.apps.googleusercontent.com',
+      'scope': 'https://www.googleapis.com/auth/youtube.force-ssl',
+      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
+  }).then(function () {
+      GoogleAuth = gapi.auth2.getAuthInstance();
+
+      // Listen for sign-in state changes.
+      GoogleAuth.isSignedIn.listen(updateSigninStatus);
+  });
+}
+
+var isAuthorized;
+var currentApiRequest;
+
+/**
+ * Store the request details. Then check to determine whether the user
+ * has authorized the application.
+ *   - If the user has granted access, make the API request.
+ *   - If the user has not granted access, initiate the sign-in flow.
+ */
+function sendAuthorizedApiRequest(requestDetails) {
+  currentApiRequest = requestDetails;
+  if (isAuthorized) {
+    // Make API request
+    // gapi.client.request(requestDetails)
+
+    // Reset currentApiRequest variable.
+    currentApiRequest = {};
+  } else {
+    GoogleAuth.signIn();
+  }
+}
+
+/**
+ * Listener called when user completes auth flow. If the currentApiRequest
+ * variable is set, then the user was prompted to authorize the application
+ * before the request executed. In that case, proceed with that API request.
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    isAuthorized = true;
+    if (currentApiRequest) {
+      sendAuthorizedApiRequest(currentApiRequest);
+    }
+  } else {
+    isAuthorized = false;
+  }
+}
+//getting profile info
+//function onSignIn(googleUser) {
+  ///  var profile = googleUser.getBasicProfile();
+    //console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    //console.log('Name: ' + profile.getName());
+    //console.log('Image URL: ' + profile.getImageUrl());
+    //console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  //}
+
+  //sign out User
+  
+
+  //function signOut() {
+    //var auth2 = gapi.auth2.getAuthInstance();
+    //auth2.signOut().then(function () {
+      //console.log('User signed out.');
+    //});
+  //}
