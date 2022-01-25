1a2271d41d38f88b115b4ad05cbc7feb4e504564
Harshit Gowda <68541048+baked1995@users.noreply.github.com>
2022-01-25 12:37:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ fetch(video_http + new URLSearchParams({
     key: api_key,
     part: 'snippet',
     chart: 'mostPopular',
-    maxResults: 10,
+    maxResults: 50,
     regionCode: 'IN'
 }))
 .then(res => res.json())
